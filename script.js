6a6d6f6bedc1b8b2e7ca84acfd1b7a2e6705d951
4Noyis <berenalpb@outlook.com>
2023-07-25 19:03:51 +0300

    commit

--- script.js
+++ script.js
@@ -5,11 +5,14 @@
 let myLibrary=[]
 let tableDiv=document.querySelector(".table")
 let addBook=document.querySelector(".add-btn")
+let cards=document.querySelector(".cards")
+
 
 let isim=document.querySelector("#title")
 let yazar=document.querySelector("#author")
 let sayfa=document.querySelector("#pages")    
-let submitbtn=document.querySelector("#submit-btn")
+let submitbtn=document.querySelector(".submit-btn")
+
 
 function Book(title, author, pages){
     this.title=title,
@@ -30,36 +33,52 @@ addBookLibrary(book2)
 addBookLibrary(book3)
 
 
-addBook.addEventListener("click", function(){
-    const newBook= new Book(`${isim.value}`,`${yazar.value}`,`${sayfa.value}`)
+
+
+submitbtn.addEventListener("click",()=> formSubmit())
+
+function formSubmit(){
+    const newBook= new Book(`${isim.value}`,`${yazar.value}`,`${sayfa.value}` )
     addBookLibrary(newBook)
-    tableDiv.innerHTML +=` 
-            <tr>
-                <td> ${newBook.title}</td>
-                <td> ${newBook.author}</td>
-                <td> ${newBook.pages}</td>
-            </tr>`;
-})
+    cards.innerHTML +=`
+    <div class="card">
+    <h2 class="book-title">${newBook.title}</h2>
+    <p class="book-author">${newBook.author}</p>
+    <p class="book-pages">${newBook.pages}</p>
+    <button class="delete-btn">Delete</button>
+    </div>
+    `
 
-// submitbtn.addEventListener("click",function(){
-//     tableDiv.innerHTML +=` 
-//             <tr>
-//                 <td> ${}</td>
-//                 <td> ${book4.author}</td>
-//                 <td> ${book4.pages}</td>
-//             </tr>`;
-// })
+    for (let i = 0; i < myLibrary.length; i++) {
+        const element = myLibrary[i];
+        console.log(element);
+    }
+}
 
+addBook.addEventListener("click",()=> showForm())
+
+function showForm(){
+    let form=document.getElementById("new-book")
+    if(form.style.display==="grid"){
+        form.style.display= "none"
+    }else{
+        form.style.display="grid"
+    }
+}
 
 
 
 for (let i = 0; i < myLibrary.length; i++) {
     const element = myLibrary[i];
-    tableDiv.innerHTML +=` 
-            <tr>
-                <td> ${element.title}</td>
-                <td> ${element.author}</td>
-                <td> ${element.pages}</td>
-            </tr>`;
+    cards.innerHTML +=`
+    <div class="card">
+    <h2 class="book-title">${element.title}</h2>
+    <p class="book-author">${element.author}</p>
+    <p class="book-pages">${element.pages}</p>
+    <button class="delete-btn">Delete</button>
+    </div>`;
+
+    console.log(myLibrary[i]);
     
 }
+
